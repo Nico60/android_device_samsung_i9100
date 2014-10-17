5a9f28cbc2737fc6bdb303a5251de9e252e84f92
Nico60 <ndhouailly@gmail.com>
2014-10-17 22:30:04 +0200

    Spirit stuff

--- spirit.mk
+++ spirit.mk
@@ -15,7 +15,7 @@
 #
 
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/spirit/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := GT-I9100
@@ -25,14 +25,14 @@ TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/spirit/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9100/full_i9100.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9100
-PRODUCT_NAME := cm_i9100
+PRODUCT_NAME := spirit_i9100
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := GT-I9100
 
